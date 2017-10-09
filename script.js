82677a038aadfe8b9def9a442f9daa125fb681fd
JD Godchaux <jd@nijel.org>
2017-10-09 18:56:55 -0400

    updating legend tray

--- script.js
+++ script.js
@@ -13,6 +13,53 @@ $(function() {
     $('.hideLegend').click(function() {
         $('.legend').hide();
         $('.showLegend').show();
-    });   
+    });
+
+    // scroll legend to the right wehn right chevron is clicked
+    var scrollPixels = 430;
+    $(".chevron-right-fade").click(function(e){
+        var leftPos = $('.legend').scrollLeft();
+        $('.legend').animate({scrollLeft: leftPos + scrollPixels }, 800);
+        // $(".chevron-left-fade").show();
+        // if ($('.legendSVG').width() < (leftPos + scrollPixels  + $('.legend').width())) {
+        //     $(".chevron-right-fade").hide();
+        // }
+    });  
+
+    // change color of chevronon hover
+    $(".chevron-right-fade").hover(function(e){
+        $('.chevron-right-wrapper').toggleClass('hover');
+    });
+
+    // scroll legend to the left when left chevron is clicked
+    $(".chevron-left-fade").click(function(e){
+        var leftPos = $('.legend').scrollLeft();
+        $('.legend').animate({scrollLeft: leftPos - scrollPixels }, 800);
+        // $(".chevron-right-fade").show();
+        // if ((leftPos - scrollPixels ) <= 0) {
+        //     $(".chevron-left-fade").hide();
+        // }
+    });  
+
+    // change color of chevronon hover
+    $(".chevron-left-fade").hover(function(e){
+        $('.chevron-left-wrapper').toggleClass('hover');
+    });
+
+    // listen for scrolling
+    $('.legend').scroll(function() {
+        var leftPos = $('.legend').scrollLeft();
+        if (leftPos <= 0) {
+            $(".chevron-left-fade").hide();
+        } else {
+            $(".chevron-left-fade").show();
+        }
+
+        if ($('.legendSVG').width() < (leftPos + $('.legend').width())) {
+            $(".chevron-right-fade").hide();
+        } else {
+            $(".chevron-right-fade").show();
+        }
+    });
 
 });
